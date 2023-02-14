b1a205d05b4ac8d1ac009a690c2ce1d8111070df
FunChosa <gilazieva.00@mail.ru>
2023-02-14 17:47:53 +0300

    1747

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,58 +1,54 @@
 import React, { useState } from "react";
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+const Counter = (props) => {
+  const { value } = props;
 
-  const formatCount = () => {
-    return count === 0 ? "empty" : count;
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
   };
 
   const getBadgeClasses = () => {
     let classes = "badge m-2 ";
-    classes += count === 0 ? "bg-warning" : "bg-primary";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
     return classes;
   };
 
-  const handleIncrement = () => {
-    setCount((prevState) => prevState + 1);
-  };
+  // const handleIncrement = () => {
+  //   console.log("handle Increment");
+  //   props.onIncrement
 
-  const handleDecrement = () => {
-    setCount((prevState) => prevState - 1);
-  };
-  const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
+  //   // setValue((prevState) => prevState + 1);
+  // };
 
-  const renderTags = () => {
-    return (
-      tags.length !== 0 &&
-      tags.map((tag) => (
-        <li
-          key={tag}
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => handleTagChange(tag)}
-        >
-          {tag}
-        </li>
-      ))
-    );
-  };
+  // const handleDecrement = () => {
+  //   console.log("handle Decrement");
+  //   props.onDecrement
 
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
+  //   // setValue((prevState) => prevState - 1);
+  // };
+
   return (
-    <>
-      <span className={getBadgeClasses()}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBadgeClasses()}>{formatValue()}</span>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onIncrement(props.id)}
+      >
         +
       </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onDecrement(props.id)}
+      >
         -
       </button>
-    </>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
